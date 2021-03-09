1efac0d257d08f83c26e7ddad7d27875f136e6d9
ApurvSinghal <apurv.singhal28@gmail.com>
2021-03-09 10:15:41 +0530

    Added Swagger related changes
    SwashBuckle Package added

--- ARMDataManager/Controllers/ValuesController.cs
+++ ARMDataManager/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace ARMDataManager.Controllers
         public IEnumerable<string> Get()
         {
             string userId = RequestContext.Principal.Identity.GetUserId();
-            return new string[] { "value1", "value2", userId2 };
+            return new string[] { "value1", "value2", userId };
         }
 
         // GET api/values/5
