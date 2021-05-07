fd4e2d21d581fdd5c0413e021f6c33cded8198a1
ApurvSinghal <apurv.singhal28@gmail.com>
2021-05-08 01:16:27 +0530

    Added Dialog Box and Permission Check.
    Checks to be sure a user has permission and prompts an alert if not.

--- ARMDataManager/Controllers/ProductController.cs
+++ ARMDataManager/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 
 namespace ARMDataManager.Controllers
 {
-    [Authorize(Roles = "Cashier")  ]
+    [Authorize(Roles = " ")  ]
     public class ProductController : ApiController
     {
         public List<ProductModel> Get()
--- ARMDesktopUI/ViewModels/SalesViewModel.cs
+++ ARMDesktopUI/ViewModels/SalesViewModel.cs
@@ -7,9 +7,11 @@ using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ARMDesktopUI.ViewModels
 {
@@ -19,19 +21,47 @@ namespace ARMDesktopUI.ViewModels
         private IConfigHelper _configHelper;
         private ISaleEndpoint _saleEndpoint;
         private IMapper _mapper;
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
+
         public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper,
-            ISaleEndpoint saleEndpoint, IMapper mapper)
+            ISaleEndpoint saleEndpoint, IMapper mapper, StatusInfoViewModel status, IWindowManager window)
         {
             _productEndpoint = productEndpoint;
             _configHelper = configHelper;
             _saleEndpoint = saleEndpoint;
-            _mapper = mapper; 
+            _mapper = mapper;
+            _status = status;
+            _window = window;
         }
 
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
-            await LoadProducts();
+            try
+            {
+                await LoadProducts();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System Error";
+
+                if(ex.Message.Equals("Unauthorized"))
+                {
+                    _status.UpdateMessage("Unauthorized Access", "You don't have the permission to intract with Sales Form.");
+                    _window.ShowDialog(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    _window.ShowDialog(_status, null, settings);
+                }
+
+                TryClose();
+            }
         }
 
         private async Task LoadProducts()
